92a30f495a16fedaa696a96d44f5cf8ee241f2f3
DishantGautam597 <gautamdishant76@gmail.com>
2025-08-24 21:40:17 +0530

    division.java

---
+++ division.java
@@ -0,0 +1,8 @@
+public class DivideNumbers {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int result = a / b;
+        System.out.println("Quotient: " + result);
+    }
+}
