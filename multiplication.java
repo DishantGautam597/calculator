a8d5fb44793961b368a6fb9c66cd765cdc40e52b
DishantGautam597 <gautamdishant76@gmail.com>
2025-08-24 21:41:28 +0530

    multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,8 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int product = a * b;
+        System.out.println("Product: " + product);
+    }
+}
